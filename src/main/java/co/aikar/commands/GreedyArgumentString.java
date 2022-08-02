0a1449acb7815c4c87b52c564b456ac0e7f5a222
Lorenzo Lapucci <halexercraft@gmail.com>
2022-08-02 16:12:10 +0200

    Update to Minestom@af9dcf258a

--- src/main/java/co/aikar/commands/GreedyArgumentString.java
+++ src/main/java/co/aikar/commands/GreedyArgumentString.java
@@ -1,11 +1,10 @@
 package co.aikar.commands;
 
-import net.minestom.server.command.builder.NodeMaker;
 import net.minestom.server.command.builder.arguments.Argument;
 import net.minestom.server.command.builder.exception.ArgumentSyntaxException;
-import net.minestom.server.network.packet.server.play.DeclareCommandsPacket;
 import net.minestom.server.utils.binary.BinaryWriter;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Argument which will take a string.
@@ -25,15 +24,15 @@ public class GreedyArgumentString extends Argument<String> {
     }
 
     @Override
-    public void processNodes(@NotNull NodeMaker nodeMaker, boolean executable) {
-        DeclareCommandsPacket.Node argumentNode = simpleArgumentNode(this, executable, false, false);
+    public String parser() {
+        return "brigadier:string";
+    }
 
-        argumentNode.parser = "brigadier:string";
-        argumentNode.properties = BinaryWriter.makeArray(packetWriter -> {
+    @Override
+    public byte @Nullable [] nodeProperties() {
+        return BinaryWriter.makeArray(packetWriter -> {
             packetWriter.writeVarInt(2); // Greedy phrase
         });
-
-        nodeMaker.addNodes(new DeclareCommandsPacket.Node[]{argumentNode});
     }
 
     @Override
