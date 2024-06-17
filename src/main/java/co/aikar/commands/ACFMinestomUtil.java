90da5e8adf77344e67566af9f1dfb4b3556cfa0b
notstevy <notstevy@ultrabuildmc.de>
2024-06-17 17:43:00 +0200

    Update to latest minestom versioon

--- src/main/java/co/aikar/commands/ACFMinestomUtil.java
+++ src/main/java/co/aikar/commands/ACFMinestomUtil.java
@@ -64,7 +64,7 @@ public class ACFMinestomUtil {
     }
 
     static boolean isValidItem(ItemStack item) {
-        return item != null && item.getMaterial() != Material.AIR && item.getAmount() > 0;
+        return item != null && item.material() != Material.AIR && item.amount() > 0;
     }
 
     static NamedTextColor[] getAllChatColors() {
